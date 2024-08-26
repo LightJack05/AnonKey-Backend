07a57a98e2ffa45079071db0a23e63b78cd12424
zak-off-zak <Zakhar_2004@inbox.ru>
2024-08-26 08:36:07 +0200

    First version of the OpenAPI Specifications for the Backend
    Only definitions of the endpoint were added. The logic of each and every
    of them should still be implemented.

--- Program.cs
+++ Program.cs
@@ -10,35 +10,84 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+// Create a User in Backend
+app.MapPost("/user", () =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+  return "Creates a user"; // A user id should be returned
 })
-.WithName("GetWeatherForecast")
+.WithName("CreateUser")
+.WithOpenApi();
+
+// Get the User Inforamtion by Id
+app.MapGet("/user{id}", (int id) =>
+{
+  return "Resturn the user information";
+})
+.WithName("GetUserInfo")
+.WithOpenApi();
+
+// Update the user's information by id
+app.MapPut("/user{id}", (int id) =>
+{
+  return "Update the user's information";
+})
+.WithName("UpdateUsersInfo")
+.WithOpenApi();
+
+
+// Delete a user by id
+app.MapDelete("/user{id}", (int id) =>
+{
+  return "Delete a user by id";
+})
+.WithName("UserDelete")
+.WithOpenApi();
+
+// Adds new key to the database
+app.MapPost("/{user_id}/keys", (int user_id) =>
+{
+  return "Adds new key to the database"; // A key's id should be returned
+})
+.WithName("AddNewKey")
+.WithOpenApi();
+
+// Returns all user's keys
+app.MapGet("/{user_id}/keys", (int user_id) =>
+{
+  return "Returns all users's keys";
+})
+.WithName("GetKeys")
+.WithOpenApi();
+
+// Get a specific user's key
+app.MapGet("/{user_id}/keys/{key_id}", (int user_id, int key_id) =>
+{
+  return "Get a specific user's key";
+})
+.WithName("GetKey")
+.WithOpenApi();
+
+// Update a specific user's key
+app.MapPut("/{user_id}/keys/{key_id}", (int user_id, int key_id) =>
+{
+  return "Update a specific user's key";
+})
+.WithName("UpdateKey")
+.WithOpenApi();
+
+
+// Delete a specific user's key
+app.MapDelete("/{user_id}/keys/{key_id}", (int user_id, int key_id) =>
+{
+  return "Delete a specific user's key";
+})
+.WithName("DeleteKey")
 .WithOpenApi();
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
