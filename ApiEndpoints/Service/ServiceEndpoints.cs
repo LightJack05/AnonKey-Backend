f64a70652c6a73eb9c795b3cd49d93ab65783632
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2024-08-27 18:55:50 +0200

    Formatted code to adhrere to .NET style guidelines

--- ApiEndpoints/Service/ServiceEndpoints.cs
+++ ApiEndpoints/Service/ServiceEndpoints.cs
@@ -1,8 +1,10 @@
 namespace KeyShare_Backend.ApiEndpoints.Service;
 
-public static class ServiceEndpoints{
-	public static void MapEndpoints(WebApplication app){
-		app.MapGet("/service/ping", Ping.GetPing);
-	}	 
+public static class ServiceEndpoints
+{
+    public static void MapEndpoints(WebApplication app)
+    {
+        app.MapGet("/service/ping", Ping.GetPing);
+    }
 
 }
