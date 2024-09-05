5ea31b1d993a11df5946567803aa43e672fedc95
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2024-09-05 18:52:50 +0200

    Changed logout request method to delete

--- ApiEndpoints/Authentication/EndpointSetup.cs
+++ ApiEndpoints/Authentication/EndpointSetup.cs
@@ -5,7 +5,7 @@ public static class EndpointSetup
     public static void MapEndpoints(WebApplication app)
     {
         app.MapPost("/authentication/login", null);
-        app.MapPut("/authentication/logout", null);
+        app.MapDelete("/authentication/logout", null);
         app.MapPut("/authentication/changePassword", null);
     }
 }
