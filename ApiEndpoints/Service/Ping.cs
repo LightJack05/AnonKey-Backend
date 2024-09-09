30ae6e452ebefca00cece95b0f0cf7d9ab8e7431
MLB-on <marc-l.brueggemann@gmx.de>
2024-09-09 13:47:41 +0200

    Reverted Ping to original state

--- ApiEndpoints/Service/Ping.cs
+++ ApiEndpoints/Service/Ping.cs
@@ -1,8 +1,8 @@
 namespace AnonKey_Backend.ApiEndpoints.Service;
 public static class Ping
 {
-    public static void GetPing()
+    public static string GetPing()
     {
-        
+        return "pong";
     }
 }
