34386d4439396131094f4b36341becad13d60cd2
MLB-on <marc-l.brueggemann@gmx.de>
2024-09-09 13:27:26 +0200

    Added sceleton for ApiEndpoints/Authentication

--- ApiEndpoints/Authentication/Login.cs
+++ ApiEndpoints/Authentication/Login.cs
@@ -1,1 +1,9 @@
 namespace AnonKey_Backend.ApiEndpoints.Authentication;
+
+public static class Login
+{
+    public static void PostLogin()
+    {
+        
+    }
+}
--- ApiEndpoints/Authentication/Logout.cs
+++ ApiEndpoints/Authentication/Logout.cs
@@ -1,1 +1,9 @@
 namespace AnonKey_Backend.ApiEndpoints.Authentication;
+
+public static class Logout
+{
+    public static void DeleteLogout()
+    {
+        
+    }
+}
