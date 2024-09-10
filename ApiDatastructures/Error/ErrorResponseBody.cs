670ca29bd32864943d4070a5317248b22fc67df9
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2024-09-10 12:32:30 +0200

    Added error response body

---
+++ ApiDatastructures/Error/ErrorResponseBody.cs
@@ -0,0 +1,7 @@
+namespace AnonKey_Backend.ApiDatastructures.Error;
+
+public class ErrorResponseBody{
+    public string Message {get; set;}
+    public string Detail {get; set;}
+    public int InternalCode {get; set;}
+}
