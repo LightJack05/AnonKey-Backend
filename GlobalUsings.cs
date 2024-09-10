4734075740cde3d078b62db259feef86cb77567b
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2024-09-10 12:33:48 +0200

    Added results dependency

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -3,6 +3,7 @@ global using System.Collections.Generic;
 global using System.Linq;
 global using Microsoft.AspNetCore.Builder;
 global using Microsoft.AspNetCore.Http;
+global using Microsoft.AspNetCore.Http.HttpResults;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Hosting;
 
