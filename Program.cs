c63d9b1628472b601f49426dfc103ea89a2c3027
zak-off-zak <Zakhar_2004@inbox.ru>
2024-09-14 06:57:15 +0200

    Updates to .gitignore, Program.cs and location of the database.db

--- Program.cs
+++ Program.cs
@@ -52,7 +52,7 @@ public class Program
         });
 
         // Entity Framework / Database
-        builder.Services.AddEntityFrameworkSqlite().AddDbContext<APIContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("ApiDatabase"))); 
+        builder.Services.AddEntityFrameworkSqlite().AddDbContext<AnonKey_Backend.Models.APIContext>(opt => opt.UseSqlite(builder.Configuration.GetConnectionString("ApiDatabase"))); 
 
         // Authentication
         builder.Services.AddSingleton<Authentication.TokenService>();
