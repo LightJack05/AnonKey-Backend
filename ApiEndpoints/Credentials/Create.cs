97b195f2a3316b2e8a029ba025385260c1e6b2cb
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2024-09-16 13:39:30 +0200

    Fixed some datastructures for requests.

--- ApiEndpoints/Credentials/Create.cs
+++ ApiEndpoints/Credentials/Create.cs
@@ -10,7 +10,7 @@ public static class Create
     /// Creates a new credential object.
     /// </summary>
     public static Microsoft.AspNetCore.Http.HttpResults.Results<
-        Ok<ApiDatastructures.Credentials.Create.CredentialsCreateResponseBody>,
+        Ok,
         BadRequest<ApiDatastructures.Error.ErrorResponseBody>>
      PostCreate(ApiDatastructures.Credentials.Create.CredentialsCreateRequestBody requestBody, ClaimsPrincipal user)
     {
