177288315e7421e7bdb4f6b52e742a7f2d5a74ad
zak-off-zak <Zakhar_2004@inbox.ru>
2024-09-17 15:38:29 +0200

    First version of credential crete without any strong checks

--- ApiEndpoints/Credentials/Create.cs
+++ ApiEndpoints/Credentials/Create.cs
@@ -14,6 +14,38 @@ public static class Create
         BadRequest<ApiDatastructures.Error.ErrorResponseBody>>
      PostCreate(ApiDatastructures.Credentials.Create.CredentialsCreateRequestBody requestBody, ClaimsPrincipal user, Data.DatabaseHandle databaseHandle)
     {
-        throw new NotImplementedException(); 
+        databaseHandle.Database.EnsureCreated();
+        if (requestBody.Credential is null || requestBody.Credential.Uuid is null || requestBody.Credential.Password is null || requestBody.Credential.PasswordSalt is null || requestBody.Credential.Username is null || requestBody.Credential.UsernameSalt is null || requestBody.Credential.WebsiteUrl is null || requestBody.Credential.Note is null || requestBody.Credential.DisplayName is null || requestBody.Credential.FolderUuid is null)
+        {
+            return TypedResults.BadRequest(new ApiDatastructures.Error.ErrorResponseBody()
+            {
+                Message = "A parameter in the request was null",
+                Detail = "One of the parameters in the request was null. This is not allowed, please fill in all parameters.",
+                InternalCode = 0x4
+            });
+        }
+
+        AnonKey_Backend.Models.Credential NewCredential = new Models.Credential()
+        {
+            // Not sure about the Uuids, in the comments of RequsetBody it is written, the the Uuid is the Uuid of the Credential, but I supose it should be the Uuid of the user
+            Uuid = Guid.NewGuid().ToString(),
+            UserUuid = requestBody.Credential.Uuid,
+            FolderUuid = requestBody.Credential.FolderUuid,
+            Password = requestBody.Credential.Password,
+            PasswordSalt = requestBody.Credential.PasswordSalt,
+            Username = requestBody.Credential.Username,
+            UsernameSalt = requestBody.Credential.UsernameSalt,
+            WebsiteUrl = requestBody.Credential.WebsiteUrl,
+            Note = requestBody.Credential.Note,
+            DisplayName = requestBody.Credential.DisplayName,
+            // Not sure about the Timestamps, especially DeletedTimestamp
+            CreatedTimestamp = (long)DateTime.UtcNow.Subtract(DateTime.UnixEpoch).TotalSeconds,
+            ChangedTimestamp = (long)DateTime.UtcNow.Subtract(DateTime.UnixEpoch).TotalSeconds,
+            DeletedTimestamp = 0,
+        };
+
+        databaseHandle.Credentials.Add(NewCredential);
+        databaseHandle.SaveChanges();
+        return TypedResults.Ok();
     }
 }
