0da17e722737e8d9433c1ef52d16ced3a696150c
zak-off-zak <Zakhar_2004@inbox.ru>
2024-09-18 21:48:22 +0200

    Delete Endpoint updated

--- ApiEndpoints/Credentials/Delete.cs
+++ ApiEndpoints/Credentials/Delete.cs
@@ -15,36 +15,37 @@ public static class Delete
             DeleteDelete(string credentialUuid, ClaimsPrincipal user, Data.DatabaseHandle databaseHandle)
     {
         databaseHandle.Database.EnsureCreated();
-        if (credentialUuid is null)
+        if (String.IsNullOrEmpty(credentialUuid))
         {
             return TypedResults.BadRequest(new ApiDatastructures.Error.ErrorResponseBody()
             {
-                Message = "A credentialUuid parameter in the request was null",
-                Detail = "credentialUuid parameter in the request was null. This is not allowed, please fill in all parameters.",
+                Message = "A credentialUuid in the request was null or an empty string",
+                Detail = "credentialUuid parameter in the request was null or an empty string. This is not allowed, please fill in all parameters.",
                 InternalCode = 0x4
             });
         }
 
-        if (credentialUuid == "")
-        {
-            return TypedResults.BadRequest(new ApiDatastructures.Error.ErrorResponseBody()
-            {
-                Message = "A credentialUuid parameter in the request was an empty stirng",
-                Detail = "One of the following parameters was an empty string: credentialUuid. This is not allowed, please make sure to provide a valid input.",
-                InternalCode = 0x5
-            });
-        }
 
         if (!databaseHandle.Credentials.Any(c => c.Uuid == credentialUuid))
         {
             return TypedResults.NotFound(new ApiDatastructures.Error.ErrorResponseBody()
             {
-                Message = "No credetial with this Uuid was not found in the database",
-                Detail = "No credetial with the provided credentialUuid was not found. Please make sure the correct credentialUuid is provided.",
+                Message = "No credential with this Uuid was not found in the database",
+                Detail = "No credential with the provided credentialUuid was not found. Please make sure the correct credentialUuid is provided.",
                 InternalCode = 0x6
             });
         }
-        databaseHandle.Credentials.Remove(databaseHandle.Credentials.SingleOrDefault(c => c.Uuid == credentialUuid));
+
+        if (databaseHandle.Users.Single(u => u.Username == user.Identity.Name).Uuid != databaseHandle.Credentials.Single(c => c.Uuid == credentialUuid).UserUuid)
+        {
+            return TypedResults.BadRequest(new ApiDatastructures.Error.ErrorResponseBody()
+            {
+                Message = "This user does not have access to this credential",
+                Detail = "The credential with the provided uuid does not belong to this user. This is not allowed, please make sure to provide a valid input.",
+                InternalCode = 0x7
+            });
+        }
+        databaseHandle.Credentials.Single(c => c.Uuid == credentialUuid).DeletedTimestamp = (long)DateTime.UtcNow.Subtract(DateTime.UnixEpoch).TotalSeconds;
         databaseHandle.SaveChanges();
         return TypedResults.Ok();
     }
