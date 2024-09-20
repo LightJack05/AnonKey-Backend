f99beb9ab4d025bd945fedb43ae6b342cd59ec1b
MLB-on <marc-l.brueggemann@gmx.de>
2024-09-20 17:19:16 +0200

    added BadRequest and CreateNewFolder

--- ApiEndpoints/Folders/Create.cs
+++ ApiEndpoints/Folders/Create.cs
@@ -1,3 +1,8 @@
+using System.Text.RegularExpressions;
+using AnonKey_Backend.ApiDatastructures.Folders.Create;
+using AnonKey_Backend.Data;
+
+
 namespace AnonKey_Backend.ApiEndpoints.Folders;
 
 /// <summary>
@@ -14,6 +19,40 @@ public static class Create
         BadRequest<ApiDatastructures.Error.ErrorResponseBody>>
             PostCreate(ApiDatastructures.Folders.Create.FoldersCreateRequestBody requestBody, ClaimsPrincipal user, Data.DatabaseHandle databaseHandle)
     {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+
+        databaseHandle.Database.EnsureCreated();
+        if (requestBody.Folder.Name is null)
+        {
+            return TypedResults.BadRequest(new ApiDatastructures.Error.ErrorResponseBody()
+            {
+                Message = "The a parameter in the request was null",
+                Detail = "One of the parameters in the request was null. This is not allowed, please fill in all parameters.",
+                InternalCode = 0x4
+            });
+        }
+
+        string token = CreateNewFolder(requestBody, user, databaseHandle);
+
+        return TypedResults.Ok(new ApiDatastructures.Folders.Create.FoldersCreateResponseBody
+        {
+            FolderUuid = token
+        });
+    }
+
+    private static string CreateNewFolder(FoldersCreateRequestBody requestBody, ClaimsPrincipal user, DatabaseHandle databaseHandle)
+    {
+        Models.Folder folder = new()
+        {
+            Uuid = Guid.NewGuid().ToString(),
+            UserUuid = user.FindFirst(ClaimTypes.NameIdentifier)?.Value, //weiß nicht ob das so richtig ist
+            DisplayName = requestBody.Folder.Name,
+            Icon = requestBody.Folder.Icon.ToString()
+        };
+        
+        databaseHandle.Folders.Add(folder);
+        databaseHandle.SaveChanges();
+
+        return folder.Uuid;
     }
 }
