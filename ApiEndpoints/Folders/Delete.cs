ec1d5fccf5df700e7f356123ec7638fd82f18d59
MLB-on <marc-l.brueggemann@gmx.de>
2024-09-24 11:46:09 +0200

    added BadRequest and NotFound and DeleteFolder Method

--- ApiEndpoints/Folders/Delete.cs
+++ ApiEndpoints/Folders/Delete.cs
@@ -1,3 +1,6 @@
+using AnonKey_Backend.Data;
+using AnonKey_Backend.Models;
+
 namespace AnonKey_Backend.ApiEndpoints.Folders;
 
 /// <summary>
@@ -15,6 +18,35 @@ public static class Delete
         BadRequest<ApiDatastructures.Error.ErrorResponseBody>>
             DeleteDelete(string folderUuid, ClaimsPrincipal user, Data.DatabaseHandle databaseHandle)
     {
-        throw new NotImplementedException();
+        databaseHandle.Database.EnsureCreated();
+        if (String.IsNullOrEmpty(folderUuid))
+        {
+            return TypedResults.BadRequest(new ApiDatastructures.Error.ErrorResponseBody()
+            {
+                Message = "The a parameter in the request was null",
+                Detail = "One of the parameters in the request was null. This is not allowed, please fill in all parameters.",
+                InternalCode = 0x4
+            });
+        }
+
+        if (databaseHandle.Folders.Any(f => f.Uuid == folderUuid))
+        {
+            return TypedResults.NotFound(new ApiDatastructures.Error.ErrorResponseBody()
+            {
+                Message = "The folder does not exist",
+                Detail = "The folder does not exist. Please check the folder uuid.",
+                InternalCode = 0x6
+            });
+        }
+
+        DeleteFolder(folderUuid, user, databaseHandle);
+        databaseHandle.SaveChanges();
+        return TypedResults.Ok();
+    }
+
+    private static void DeleteFolder(string folderUuid, ClaimsPrincipal user, DatabaseHandle databaseHandle)
+    {
+        User userObject = databaseHandle.Users.First(u => u.Username == user.Identity.Name);
+        databaseHandle.Folders.RemoveRange(databaseHandle.Folders.Where(f => f.Uuid == folderUuid && f.UserUuid == userObject.Uuid));
     }
 }
