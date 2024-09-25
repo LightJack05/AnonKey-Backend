eb963cb085b5300f0dbe814b94356297be8d9ac5
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2024-09-25 14:51:58 +0200

    Added user change password endpoint

--- ApiEndpoints/Authentication/ChangePassword.cs
+++ ApiEndpoints/Authentication/ChangePassword.cs
@@ -1,3 +1,6 @@
+using AnonKey_Backend.Data;
+using AnonKey_Backend.Models;
+
 namespace AnonKey_Backend.ApiEndpoints.Authentication;
 
 /// <summary>
@@ -13,6 +16,42 @@ public static class ChangePassword
         BadRequest<ApiDatastructures.Error.ErrorResponseBody>>
             PutChangePassword(ApiDatastructures.Authentication.ChangePassword.AuthenticationChangePasswordRequestBody requestBody, ClaimsPrincipal user, Data.DatabaseHandle databaseHandle)
     {
-        throw new NotImplementedException();
+        databaseHandle.Database.EnsureCreated();
+
+        User? userFromDatabase = databaseHandle.Users.Where(u => u.Username == user.Identity.Name).FirstOrDefault();
+
+        // If no user is found matching the redentials, return 404.
+        if (userFromDatabase == null)
+        {
+            return TypedResults.BadRequest(new ApiDatastructures.Error.ErrorResponseBody()
+            {
+                Message = "User not found.",
+                Detail = "The lookup for the username in the request did not yeild any results. Please make sure there is a user with the specified name.",
+                InternalCode = 0x1
+            });
+        }
+
+        if (!Cryptography.PasswordHashing.isPasswordValid(requestBody.KdfResultOldPassword, userFromDatabase))
+        {
+            return TypedResults.BadRequest(new ApiDatastructures.Error.ErrorResponseBody
+            {
+                Message = "Invalid password",
+                Detail = "The password did not match the password used before. Please ensure the password is correct.",
+                InternalCode = 0x1
+            });
+        }
+
+        ChangeUserPassword(userFromDatabase, requestBody.KdfResultNewPassword, databaseHandle);
+
+        databaseHandle.SaveChanges();
+        return TypedResults.Ok();
+    }
+
+    private static void ChangeUserPassword(User user, string newPassword, DatabaseHandle databaseHandle)
+    {
+        string passwordSalt = Cryptography.Generators.NewRandomString(Configuration.Settings.UserPasswordSaltLength);
+
+        user.PasswordSalt = passwordSalt;
+        user.PasswordHash = Cryptography.PasswordHashing.HashPassword(newPassword, passwordSalt);
     }
 }
