931d0cf2d7138db8583c6a1b2414fcb5fe2d1050
MLB-on <marc-l.brueggemann@gmx.de>
2024-10-01 15:35:19 +0200

    Added BadRequest, NotFound, OK and UpdateFolder-Method

--- ApiEndpoints/Folders/Update.cs
+++ ApiEndpoints/Folders/Update.cs
@@ -1,3 +1,7 @@
+using AnonKey_Backend.ApiDatastructures.Folders.Update;
+using AnonKey_Backend.Data;
+using AnonKey_Backend.Models;
+
 namespace AnonKey_Backend.ApiEndpoints.Folders;
 
 /// <summary>
@@ -14,7 +18,43 @@ public static class Update
         BadRequest<ApiDatastructures.Error.ErrorResponseBody>>
             PutUpdate(ApiDatastructures.Folders.Update.FoldersUpdateRequestBody requestBody, ClaimsPrincipal user, Data.DatabaseHandle databaseHandle)
     {
-        throw new NotImplementedException();
+        databaseHandle.Database.EnsureCreated();
+
+        if (user == null)
+        {
+            return TypedResults.BadRequest(new ApiDatastructures.Error.ErrorResponseBody()
+            {
+                Message = "The user is null",
+                Detail = "The user is null, therefore no data extraction is possible",
+                InternalCode = 0x4
+            });
+        }
+
+        User userObject = databaseHandle.Users.FirstOrDefault(u => u.Username == user.Identity.Name);
+        Folder folder = databaseHandle.Folders.FirstOrDefault(f => f.Uuid == requestBody.Folder.Uuid);
+        if (folder == null || userObject == null || folder.UserUuid != userObject.Uuid)
+        {
+            return TypedResults.NotFound(new ApiDatastructures.Error.ErrorResponseBody()
+            {
+                Message = "The folder does not exist",
+                Detail = "The folder with the given UUID does not exist.",
+                InternalCode = 0x6
+            });
+        }
+
+        UpdateFolder(requestBody, folder);
+        databaseHandle.SaveChanges();
+        Folder updatedFolder = databaseHandle.Folders.Single(f => f.Uuid == requestBody.Folder.Uuid);
         
+        return TypedResults.Ok(new ApiDatastructures.Folders.Update.FoldersUpdateResponseBody{
+            FolderUuid = updatedFolder.Uuid
+        });
+    }
+
+    private static void UpdateFolder(FoldersUpdateRequestBody requestBody, Folder folder)
+    {
+        folder.Uuid = requestBody.Folder.Uuid;
+        folder.DisplayName = requestBody.Folder.Name;
+        folder.Icon = requestBody.Folder.Icon;
     }
 }
