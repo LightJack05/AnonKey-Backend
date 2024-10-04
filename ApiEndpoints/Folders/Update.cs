bb03077f33f638629a540a2f5d484de98caa78e1
MLB-on <marc-l.brueggemann@gmx.de>
2024-10-04 09:44:15 +0200

    Refactor UpdateFolder method and handle null user and folder cases

--- ApiEndpoints/Folders/Update.cs
+++ ApiEndpoints/Folders/Update.cs
@@ -1,3 +1,4 @@
+using System.Reflection.Metadata.Ecma335;
 using AnonKey_Backend.ApiDatastructures.Folders.Update;
 using AnonKey_Backend.Data;
 using AnonKey_Backend.Models;
@@ -20,40 +21,47 @@ public static class Update
     {
         databaseHandle.Database.EnsureCreated();
 
-        if (user == null)
+        if (String.IsNullOrEmpty(requestBody.Folder.Uuid) || String.IsNullOrEmpty(requestBody.Folder.Name))
         {
             return TypedResults.BadRequest(new ApiDatastructures.Error.ErrorResponseBody()
             {
-                Message = "The user is null",
-                Detail = "The user is null, therefore no data extraction is possible",
+                Message = "A parameter in the request was null or an empty string",
+                Detail = "One of the parameters in the request was null or an empty string. This is not allowed, please fill in all parameters.",
                 InternalCode = 0x4
             });
         }
 
         User userObject = databaseHandle.Users.FirstOrDefault(u => u.Username == user.Identity.Name);
+        if (userObject == null)
+        {
+            return TypedResults.NotFound(new ApiDatastructures.Error.ErrorResponseBody{
+                Message = "The user does not exist",
+                Detail = "The user does not exist in the database, he might have been deleted.",
+                InternalCode = 0x5
+            });
+        }
+
         Folder folder = databaseHandle.Folders.FirstOrDefault(f => f.Uuid == requestBody.Folder.Uuid);
-        if (folder == null || userObject == null || folder.UserUuid != userObject.Uuid)
+        if (folder == null || folder.UserUuid != userObject.Uuid)
         {
             return TypedResults.NotFound(new ApiDatastructures.Error.ErrorResponseBody()
             {
                 Message = "The folder does not exist",
-                Detail = "The folder with the given UUID does not exist.",
+                Detail = "The folder with the given UUID does not exist, or does not belong to the user.",
                 InternalCode = 0x6
             });
         }
 
         UpdateFolder(requestBody, folder);
         databaseHandle.SaveChanges();
-        Folder updatedFolder = databaseHandle.Folders.Single(f => f.Uuid == requestBody.Folder.Uuid);
         
         return TypedResults.Ok(new ApiDatastructures.Folders.Update.FoldersUpdateResponseBody{
-            FolderUuid = updatedFolder.Uuid
+            FolderUuid = folder.Uuid
         });
     }
 
     private static void UpdateFolder(FoldersUpdateRequestBody requestBody, Folder folder)
     {
-        folder.Uuid = requestBody.Folder.Uuid;
         folder.DisplayName = requestBody.Folder.Name;
         folder.Icon = requestBody.Folder.Icon;
     }
