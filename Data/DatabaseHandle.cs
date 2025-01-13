338a637ad7f03c95bbc0df3efbe94ef7157ec2d0
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-01-13 18:30:57 +0100

    Changed string comparison

--- Data/DatabaseHandle.cs
+++ Data/DatabaseHandle.cs
@@ -43,7 +43,7 @@ public class DatabaseHandle : DbContext
     /// </summary>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        if (string.Equals(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"), "development", StringComparison.InvariantCultureIgnoreCase))
+        if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
         {
             modelBuilder.Seed();
         }
