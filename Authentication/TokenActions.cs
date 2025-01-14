4a886f23386b616fc7ce737522e34796e14d458b
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2025-01-14 16:36:23 +0100

    Added doc comments

--- Authentication/TokenActions.cs
+++ Authentication/TokenActions.cs
@@ -1,12 +1,25 @@
 namespace AnonKeyBackend.Authentication;
 
+/// <summary>
+/// Contians actions to perform on Tokens
+/// </summary>
 public class TokenActions
 {
-    public static void StoreTokenInDb(Models.Token token, Data.DatabaseHandle databaseHandle)
+    /// <summary>
+    /// Stores a refresh token into the database
+    /// </summary>
+    /// <param name="token">The token to store</param>
+    /// <param name="databaseHandle">Databasehandle to store the token to</param>
+    public static void StoreRefreshTokenInDb(Models.Token token, Data.DatabaseHandle databaseHandle)
     {
         databaseHandle.RefreshTokens.Add(token);
     }
 
+    /// <summary>
+    /// Validates if an access token is valid (and not revoked)
+    /// </summary>
+    /// <param name="token">The token to validate</param>
+    /// <param name="databaseHandle">Databasehandle to validate the token against</param>
     public static bool IsAccessTokenValid(Models.Token token, Data.DatabaseHandle databaseHandle)
     {
         if(token.TokenType != "AccessToken") throw new ArgumentOutOfRangeException(nameof(token),"The token is not an access token.");
@@ -18,6 +31,11 @@ public class TokenActions
         return true;
     }
 
+    /// <summary>
+    /// Validates if a refresh token is valid (and not revoked)
+    /// </summary>
+    /// <param name="token">The token to validate</param>
+    /// <param name="databaseHandle">Databasehandle to validate the token against</param>
     public static bool IsRefreshTokenValid(Models.Token token, Data.DatabaseHandle databaseHandle){
         if(token.TokenType != "RefreshToken") throw new ArgumentOutOfRangeException(nameof(token),"The token is not a refresh token.");
         Models.Token? tokenInDb = databaseHandle.RefreshTokens.FirstOrDefault(t => t.Uuid == token.Uuid);
